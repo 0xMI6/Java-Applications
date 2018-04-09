db41ffee131a9c72da0d22435d0bc6f4986dee75
0xMI6 <barakat_smile@yahoo.com>
2018-04-09 17:47:10 +0200

    Quadratic function calculation

---
+++ Quadratic.java
@@ -0,0 +1,26 @@
+package second;
+
+public class Quadratic {
+
+    public static void main(String[] args) { 
+        double x= Double.parseDouble(args[0]);
+        
+        double y = Double.parseDouble(args[1]);
+        
+        double equation = x*x +4.0*y;
+        double sqrt= Math.sqrt(equation);
+        double firstroot= (-x+sqrt)/2.0;
+        double firstroot2= (-x-sqrt)/2.0;
+        
+        System.out.println(firstroot);
+        System.out.println(firstroot2);
+
+
+  
+    }
+		
+	}
+
+
+	
+
